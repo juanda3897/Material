542e9df8da8aec4b388b1da5684bdd0faf376f4b
Juan <juand-escobart@unilibre.edu.co>
2019-09-13 15:50:27 -0500

    Todos los componentes trabajando, app.component.html sin diseño, muestra los componentes

--- src/app/components/formfield/formfield.component.ts
+++ src/app/components/formfield/formfield.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-formfield',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formfield.component.css']
 })
 export class FormfieldComponent implements OnInit {
+  
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+        this.email.hasError('email') ? 'Not a valid email' :
+            '';
+            
+  }
 
   constructor() { }
 
