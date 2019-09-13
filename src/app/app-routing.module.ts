a8be1c438d33b26aebe7b691a54dcd2f74eca845
Juan <juand-escobart@unilibre.edu.co>
2019-09-13 09:18:08 -0500

    Desagregación en componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import {ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,30 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FormsModule, ReactiveFormsModule} from'@angular/forms';
-import {MatAutocompleteModule, MatInputModule } from '@angular/material';
+import {MatAutocompleteModule, MatInputModule, MatCheckbox, MatCheckboxModule, MatRadioButton, MatRadioModule } from '@angular/material';
+import { AutocompleteComponent } from './components/autocomplete/autocomplete.component';
+import { CheckboxComponent } from './components/checkbox/checkbox.component';
+import { DatepickerComponent } from './components/datepicker/datepicker.component';
+import { FormfieldComponent } from './components/formfield/formfield.component';
+import { InputComponent } from './components/input/input.component';
+import { RadiobuttonComponent } from './components/radiobutton/radiobutton.component';
+import { SelectComponent } from './components/select/select.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { SlidetoggleComponent } from './components/slidetoggle/slidetoggle.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    AutocompleteComponent,
+    CheckboxComponent,
+    DatepickerComponent,
+    FormfieldComponent,
+    InputComponent,
+    RadiobuttonComponent,
+    SelectComponent,
+    SliderComponent,
+    SlidetoggleComponent,
  
   ],
   imports: [
@@ -21,7 +39,10 @@ import {MatAutocompleteModule, MatInputModule } from '@angular/material';
     FormsModule,
     ReactiveFormsModule,
     MatAutocompleteModule,
-    MatInputModule
+    MatInputModule,
+    MatCheckboxModule,
+    MatRadioModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
